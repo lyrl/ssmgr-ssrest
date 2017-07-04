bbf3fef099e7838a62722f4a39e953387cdf981a
lyrl <lyrl@me.com>
2017-07-04 17:01:17 +0800

    * 配置：初始化

--- shadowsocks/manager.py
+++ shadowsocks/manager.py
@@ -59,7 +59,9 @@ class Manager(object):
         url = 'http://%s:%s/api/comm/node/users/%s' % (
         self._config['ssmgr_backend_host'], self._config['ssmgr_backend_port'], self._config['security_key'])
 
-        data = urllib2.urlopen(url).read()
+        data = urllib2.urlopen(url)
+        logging.info("用户列表 : %s" % data)
+
         node = json.load(data)
 
         for user in node.users:
